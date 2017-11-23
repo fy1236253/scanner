c78e81c74732b6e9096453d70864864eb7491c54
fengya <zhangfengya@yunlaba.com>
2017-11-23 12:08:11 +0800

    second ticket

--- src/model/deal_bat.go
+++ src/model/deal_bat.go
@@ -92,6 +92,7 @@ func BatImageRecognition(base64Str string) (string, int) {
 		log.Println(err)
 		return "", 0
 	}
+	log.Println(resp)
 	types := RecongnitionType(resp)
 	log.Println(types)
 	return resp, types
